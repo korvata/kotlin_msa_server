8bf1b1690d1458363a7692afac193cfb4f72f69f
korvata <korvata@naver.com>
2022-10-15 01:55:24 +0900

    catalog-service : spring security 의존성 제거

--- server/api/catalog/build.gradle.kts
+++ server/api/catalog/build.gradle.kts
@@ -22,8 +22,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("com.auth0:java-jwt:3.19.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.modelmapper:modelmapper:3.0.0")
--- server/api/catalog/src/main/kotlin/catalog/v1/controller/CatalogController.kt
+++ server/api/catalog/src/main/kotlin/catalog/v1/controller/CatalogController.kt
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/catalog-service/v1")
