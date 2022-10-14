e898320de89b19a0ecfed64be6495a418eaf5291
korvata <korvata@naver.com>
2022-10-15 01:26:46 +0900

    catalog-service

---
+++ server/api/catalog/src/main/kotlin/catalog/CatalogServiceApplication.kt
@@ -0,0 +1,13 @@
+package catalog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+class CatalogServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CatalogServiceApplication>(*args)
+}
